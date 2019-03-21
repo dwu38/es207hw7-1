6fe69cb8fa10d8267ce3d98a9c5432fecb01c91b
dwu38 <48276517+dwu38@users.noreply.github.com>
2019-03-20 17:19:38 -0700

    Update Q9 sans santa search function.R

--- Q9 sans santa search function.R
+++ Q9 sans santa search function.R
@@ -6,4 +6,4 @@ match <- str_c(sans, collapse = "|")
 has_san <- str_subset(loc$'Site Name', match)
 has_san
 
-#Yours automatically returned all the Sans unlike mine, so yay:)
+#Yours automatically returned all the Sans unlike mine, so yay:) But was it supposed to include the counties?
