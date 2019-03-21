c4d658c4d072711c16e45ee34d9abb6fcfec72ab
dwu38 <48276517+dwu38@users.noreply.github.com>
2019-03-20 17:14:22 -0700

    Update Q10 complete address search function.R

--- Q10 complete address search function.R
+++ Q10 complete address search function.R
@@ -5,3 +5,5 @@
 
 complete <- length(loc$Address) - sum(str_detect(loc$Address, "^\\d") & str_detect(loc$`Zip Code`, "\\d{5}"), na.rm = T)
 complete
+
+# I would have loaded all needed libraries as well as the xls file if I wanted this code to stand on its own. But yours automatically gave an answer unlike mine!
